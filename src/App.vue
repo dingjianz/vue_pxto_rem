3b9e0176ee896499df11ebb8df9263aede0f0f59
dingjianz <573828395@qq.com>
2020-06-10 22:06:16 +0800

    20200610

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+
+    <div class="box"></div>
+  </div>
+</template>
+
+<style lang="scss">
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    // color: #2c3e50;
+    color:red;
+    font-size: 15px;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+.box {
+  width: 200px;
+  height: 200px;
+  border: 1px solid #000;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img class="img" alt="Vue logo" src="../assets/logo.png" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "home"
+};
+</script>
+
+<style lang="scss" scoped>
+.img {
+  width: 200px;
+}
+</style>
